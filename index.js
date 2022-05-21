86ae50c352eed2b3c26dba04b24e2527b554fd72
AronThantos <97336117+AronThantos@users.noreply.github.com>
2022-05-21 19:03:43 +0700

    cuoi khoa

---
+++ index.js
@@ -0,0 +1,67 @@
+// // let a = "String AAA";
+// // let b = a;
+// // let c = b;
+// // b = b + "BBB"
+// // c = c + "CCC"
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+// let menuCofee = [
+//     "Capuchinoi",
+//     "Bac siu",
+//     "Americano",
+//     "Black",
+//     "Nau Khoi",
+//     "Ca phe trung"]
+// // 
+// // let copyMenu = [];
+// // for( let k = 0; k < menuCofee.length; k++){
+// //     copyMenu[k] = menuCofee[k];
+// // }
+// // copyMenu.push("Pikachu")
+// // console.log(menuCofee);
+// // console.log(copyMenu);
+// // for (i in menuCofee){ 
+// //     console.log(i, menuCofee[i]);
+// // }
+
+// let info = {
+//     height: " 1.78m",
+// }
+// info.height = "1,79m"
+// info["vailoz"] = "vaicc"
+// console.log(info);
+
+// for(let i = 0; i<100; i ++){
+//   if (i % 2 === 0){console.log(i)
+//   }}
+//   let inputNumber = 13;
+//   let isPrime = true;
+//   for(
+//     let i = 2; i <= inputNumber; i++
+//   ) {
+//     if(inputNumber % inputNumber === 0)
+//     {
+//       isPrime = false;
+//       console.log("uoc so dau ten la : ", i);
+//       break;
+//     }
+//   }
+//   if(isPrime) {
+//     console.log("so ban nhap la so nguyen to");
+//   }
+//   else {
+//     console.log("so ban nhap la hop so");
+//   }
+ let menu1 = [
+     "pikachu",
+     "pokemon",
+     "salamande"
+ ];
+ let menu3 = {
+     height: "1.88m",
+     dob: "asdads"
+ }
+delete menu3.dob
+console.log(menu3);
